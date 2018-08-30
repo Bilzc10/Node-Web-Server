0a28946de31bad67e5e3afff6ab1b26e070ef6d2
Bilzc10 <31910622+Bilzc10@users.noreply.github.com>
2018-08-30 15:18:38 -0400

    Update Files

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 
+  //Options file - for modifying settings
 var options = require("./options.json");
 
-app.use(express.static(path.join(__dirname, options.webDir)));
+app.use(express.static(path.join(__dirname, options.webDir))); //Serve any files requested that exist in web directory
+
+server.listen(options.port);
+
+console.log("Server Started. Listening on port " + options.port);
